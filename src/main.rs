bb8f196a98ffb0c73dd07bc0df618d75713e9cf7
Richard Ekle <rick@ekle.org>
2026-06-19 17:54:30 -0400

    Added sample parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+/// RCL - The Rust Command Line
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("RCL: The Rust Command Line tool.");
+
+    //let cli_args = std::env::args();
+
+    let args = Cli::parse();
+    println!("Parsed args: {args:?}");
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
